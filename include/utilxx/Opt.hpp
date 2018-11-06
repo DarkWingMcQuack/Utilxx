dbecb82e750c76d402e3beab7bb3a503880870cb
DarkWingMcQuack <np-hardass@protonmail.com>
2018-11-06 23:51:25 +0100

    added onError to Opt

--- include/utilxx/Opt.hpp
+++ include/utilxx/Opt.hpp
@@ -210,6 +210,36 @@ public:
         return std::move(*this);
     }
 
+    template<class Func>
+    constexpr auto onError(Func&& func) & -> Opt<T>&
+    {
+        using FuncRet = std::invoke_result_t<Func>;
+
+        static_assert(std::is_void_v<FuncRet>,
+                      "return of onError function must be void");
+
+        if(!*this) {
+            std::invoke(std::forward<Func>(func));
+        }
+
+        return *this;
+    }
+
+    template<class Func>
+    constexpr auto onError(Func&& func) && -> Opt<T>&&
+    {
+        using FuncRet = std::invoke_result_t<Func>;
+
+        static_assert(std::is_void_v<FuncRet>,
+                      "return of onError function must be void");
+
+        if(!*this) {
+            std::invoke(std::forward<Func>(func));
+        }
+
+        return std::move(*this);
+    }
+
     constexpr auto flatten() &&
     {
         if constexpr(is_opt_v<T>) {
