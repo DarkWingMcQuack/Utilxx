bb3a966870da7670b65db7d62cf19a0c64bb8a50
DarkWingMcQuack <np-hardass@protonmail.com>
2018-11-30 15:17:56 +0100

    added some implementations for erase_if

--- include/utilxx/Algorithm.hpp
+++ include/utilxx/Algorithm.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
-namespace utilxx {
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
-namespace {
+namespace utilxx {
 
 template<class InputIterator,
          class OutputIterator,
@@ -22,6 +27,80 @@ OutputIterator transform_if(InputIterator first,
     return result;
 }
 
+template<class Key,
+         class T,
+         class Compare,
+         class Alloc,
+         class Pred>
+void erase_if(std::map<Key, T, Compare, Alloc>& c, Pred pred)
+{
+    for(auto i = c.begin(), last = c.end(); i != last;) {
+        if(pred(*i)) {
+            i = c.erase(i);
+        } else {
+            ++i;
+        }
+    }
 }
+
+template<class T,
+         class Compare,
+         class Alloc,
+         class Pred>
+void erase_if(std::set<T, Compare, Alloc>& c, Pred pred)
+{
+    for(auto i = c.begin(), last = c.end(); i != last;) {
+        if(pred(*i)) {
+            i = c.erase(i);
+        } else {
+            ++i;
+        }
+    }
+}
+
+template<class Key,
+         class T,
+         class Hash,
+         class KeyEqual,
+         class Alloc,
+         class Pred>
+void erase_if(std::unordered_map<Key, T, Hash, KeyEqual, Alloc>& c, Pred pred)
+{
+    for(auto i = c.begin(), last = c.end(); i != last;) {
+        if(pred(*i)) {
+            i = c.erase(i);
+        } else {
+            ++i;
+        }
+    }
+}
+
+template<class Key,
+         class Hash,
+         class KeyEqual,
+         class Alloc,
+         class Pred>
+void erase_if(std::unordered_set<Key, Hash, KeyEqual, Alloc>& c, Pred pred)
+{
+    for(auto i = c.begin(), last = c.end(); i != last;) {
+        if(pred(*i)) {
+            i = c.erase(i);
+        } else {
+            ++i;
+        }
+    }
+}
+
+template<class T,
+         class Alloc,
+         class Pred>
+void erase_if(std::vector<T, Alloc>& c, Pred pred)
+{
+    c.erase(std::remove_if(c.begin(),
+                           c.end(),
+                           pred),
+            c.end());
+}
+
 
 } // namespace utilxx
